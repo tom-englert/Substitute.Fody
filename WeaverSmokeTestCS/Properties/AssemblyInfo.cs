b4ab63d4ae8a6f7b9d597f4bb9fc35bead44280e
tom-englert <mail@tom-englert.de>
2018-03-23 11:50:11 +0100

    Add weaver smoke test

---
+++ WeaverSmokeTestCS/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Substitute;
+using WeaverSmokeTestCS;
+
+[assembly: AssemblyTitle("WeaverSmokeTestCS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeaverSmokeTestCS")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Substitute(typeof(ComponentResourceManager), typeof(MyComponentResourceManager))]
---
+++ WeaverSmokeTestCS/Substitutes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace WeaverSmokeTestCS
+{
+    internal class MyComponentResourceManager : ComponentResourceManager
+    {
+        public MyComponentResourceManager(Type t)
+        {
+        }
+
+        public new void ApplyResources(object value, string objectName)
+        {
+            base.ApplyResources(value, objectName);
+        }
+
+        public override void ApplyResources(object value, string objectName, CultureInfo culture)
+        {
+            base.ApplyResources(value, objectName, culture);
+        }
+    }
+}
